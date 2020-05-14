84934d727317e71656894d7d3e68f0c6b5030334
kevinjohnkiely <kevinjohnkiely1@hotmail.com>
2020-05-14 21:54:08 +0100

    Updates

---
+++ precache-manifest.d7e84ccdca0cee274327f5a41af06c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cf9c5f1a8b64a861e87af7522026b3",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "21976ad8f369a67945af",
+    "url": "/shopping-list/static/css/main.b463eb8c.chunk.css"
+  },
+  {
+    "revision": "7e0939cfa172faab6464",
+    "url": "/shopping-list/static/js/2.15657780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list/static/js/2.15657780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21976ad8f369a67945af",
+    "url": "/shopping-list/static/js/main.fc445ecd.chunk.js"
+  },
+  {
+    "revision": "86563f892b6afbb60368",
+    "url": "/shopping-list/static/js/runtime-main.fe7dd302.js"
+  }
+]);
